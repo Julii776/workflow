67ca00f2a8f442bdafb50a00f89cfc1608528291
Juli Sah <juli@truemark.com.np>
2025-11-11 13:44:00 +0545

    eaxmple test

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+})
+
+const customJestConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-environment-jsdom',
+    collectCoverageFrom: [
+        'app/**/*.{js,jsx,ts,tsx}',
+        'pages/**/*.{js,jsx,ts,tsx}',
+        'components/**/*.{js,jsx,ts,tsx}',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+        '!**/.next/**',
+    ],
+    coverageReporters: ['lcov', 'text', 'text-summary'],
+}
+
+module.exports = createJestConfig(customJestConfig)
