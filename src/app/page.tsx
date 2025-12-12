247f053bfcafbfcb04671ff1d9051cb41cc86965
Juli Sah <juli@truemark.tech>
2025-12-12 10:28:16 +0545

    sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Home from "@/components/Home";
 import Notes from "@/components/Notes";
+import Sample from "@/components/Sample";
 
 export default function page() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Home />
+      <Sample />
       <Notes />
     </main>
   );
---
+++ src/components/Sample.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sample = () => {
+    return (
+        // <h1>Home</h1>
+
+        <div>
+            <h1>Sample</h1>
+            <p data-testid="desc">This is the sample component.</p>
+        </div>
+    );
+};
+
+export default Sample;
